6a837f7998593f47b01967137609888c6506de0e
yebink <yebin825@gmail.com>
2021-01-19 00:54:59 +0900

    feat: useEffect 사용

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,32 +1,29 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-    const [username, setUsername] = useState('') //초기값: empty string
-    const [password, setPassword] = useState('') //초기값: empty string
 
-    const onSubmit = (event) => {
-        event.preventDefault(); //폼이 새로고침되는 것을 막아주기 위해
-        alert('submitted');
-        console.log(username, password);
+    const [count, setCount] = useState(0); //count가 변경될 때마다 리렌더링 된다.
+    const [elly, setElly] = useState(0);
+
+    useEffect(() => {
+        //이 함수 안에 있는 로직이 렌더링이 된 후 실행된다.
+        console.log('얘가 호출된다.');
+        console.log(count, elly)
+    }, [count]) //count가 변경될 때에만 이 메소드가 실행된다. elly까지 받고 싶으면 [count, elly]
+    console.log('얘가 먼저 호출되고');
+
+    useEffect(() => {
+        console.log('first rendering');
+    }, []); //이렇게 아무것도 안 넣으면 최초 1회만 실행된다.
+
+    const increment = () => {
+        setCount(count + 1);
     }
-
-    //input의 값이 변할 때마다 username 변수를 업데이트한다.
     return (
         <div className="App">
-            <form onSubmit={onSubmit}>
-            <input
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}/>
-            <br/>
-
-            <input
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
-            <br/>
-            <button type="submit">Login</button>
-            </form>
+            <h1>Elly Nelly</h1>
+            <button onClick={increment}>Click</button>
+            <button onClick={() => setElly(elly + 1)}>Click1</button>
         </div>
     );
 }
