e7446a1f6a687c13d0820d1e3c0d72804f14ed1a
yebink <yebink@woowahan.com>
2021-01-19 00:05:37 +0900

    feat: 리액트 훅스 : useState

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+    const [text, setText] = useState('elly1');
 
     const onKeyUp = (event) => {
         if (event.keyCode === 13) { //엔터이면
@@ -13,10 +15,23 @@ function App() {
         alert('submitted');
     }
 
+    // let ellyText = 'elly1';
+
+    const updateText = () => {
+        // ellyText = 'nelly1';
+        setText('nelly1');
+        console.log(text);
+    }
+
     return (
         <div className="App">
             <input onKeyUp={onKeyUp}/>
             <button onClick={onSubmit}>Submit</button>
+
+            <br/> <br/>
+
+            <span>{text}</span>
+            <button onClick={updateText}>Update</button>
         </div>
     );
 }
