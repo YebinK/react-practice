582a4849e0743da26505bc5e97ef0c98b0947cce
yebink <yebink@woowahan.com>
2021-01-18 23:53:53 +0900

    feat: 이벤트 핸들링

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Elly
-    </div>
-  );
+
+    const onKeyUp = (event) => {
+        if (event.keyCode === 13) { //엔터이면
+            onSubmit();
+        }
+        console.log('key up');
+    }
+
+    const onSubmit = () => {
+        alert('submitted');
+    }
+
+    return (
+        <div className="App">
+            <input onKeyUp={onKeyUp}/>
+            <button onClick={onSubmit}>Submit</button>
+        </div>
+    );
 }
 
 export default App;
