362b54ef25fe632d84d088fea9e26a6ce8dc1d66
yebink <yebin825@gmail.com>
2021-01-30 04:13:24 +0900

    feat: 반복문 구현

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,33 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 function App() {
 
-    const [condition, setCondition] = useState(false);
+    const movies = [
+        { id: 1, title: 'Harry Potter 1', year: 2001 },
+        { id: 2, title: 'Harry Potter 2', year: 2002 },
+        { id: 3, title: 'Harry Potter 3', year: 2003 },
+        { id: 4, title: 'Harry Potter 4', year: 2004 },
+        { id: 5, title: 'Harry Potter 5', year: 2005 },
+    ]
 
-    const toggle = () => {
-        setCondition(!condition);
-    }
-
-    useEffect(() => {
-        console.log(condition)
-    }, [condition])
-
-    const renderCondition = condition
-        ? <div>True</div>
-        : <div>False</div>
-
-    const renderCondition2 = condition
-        ? 'True'
-        : 'False'
+    const renderMovies = movies.map(movie => {
+        return (
+            <div className="movie" key={movie.id}>
+                <div className="movie-title">{movie.title}</div>
+                <div className="movie-year">{movie.year}</div>
+            </div>
+        )
+    })
 
     return (
         <div className="App">
-            <h1>Elly Nelly</h1>
-            {renderCondition}
-
-            <div> {renderCondition2} </div>
-
-            <button onClick={toggle}>Toggle</button>
+            <h1>Movie List</h1>
+            {renderMovies}
         </div>
     );
 }
