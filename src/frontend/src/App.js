92946e14f9bbab56e66391c6f89f07f2224ac51d
yebink <yebin825@gmail.com>
2021-01-30 04:22:39 +0900

    feat: Movie 컴포넌트 분리

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Movie from "./components/Movie";
 
 function App() {
 
@@ -12,10 +13,7 @@ function App() {
 
     const renderMovies = movies.map(movie => {
         return (
-            <div className="movie" key={movie.id}>
-                <div className="movie-title">{movie.title}</div>
-                <div className="movie-year">{movie.year}</div>
-            </div>
+            <Movie movie={movie} key={movie.id} />
         )
     })
 
---
+++ src/frontend/src/components/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Movie = ({ movie}) => {
+
+    return (
+        <div className="movie">
+            <div className="movie-title">{movie.title}</div>
+            <div className="movie-year">{movie.year}</div>
+        </div>
+    );
+}
+
+export default Movie;
